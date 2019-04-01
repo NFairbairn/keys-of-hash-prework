d2f661663c3bfacfe72e71839ba224e98c69c831
Noah Fairbairn <noahfairbairn@gmail.com>
2019-04-01 18:20:14 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,11 +3,8 @@ require 'pry'
 class Hash
   def keys_of(*arguments)
     # code goes here
-    #binding.pry
     key_arry = []
     animals.each do |key,value|
-      #key.each do |key_2, value_2|
-      #binding.pry
         if value == arguments
           key_arry << key
 
